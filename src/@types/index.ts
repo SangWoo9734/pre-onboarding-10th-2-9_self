d6a797fabedadb170c51dac108a2fd75f8ea7df9
Parksangwoo <psu8430@gmail.com>
2023-05-04 04:51:29 +0900

    Feat: 타입 관련 파일 추가

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,19 @@
-export {};
+export interface SearchUnitType {
+  id: number;
+  name: string;
+}
+
+export interface ApiResponseType {
+  word: string;
+  isSuccess: boolean;
+  data: SearchUnitType[];
+  message?: string;
+  callTime: number;
+}
+
+export interface autoCompleteSliceInitialStateType {
+  focusIndex: number;
+  isCollapsed: boolean;
+  currentAutoCompleteResult: ApiResponseType;
+  autoCompleteHistory: ApiResponseType[];
+}
